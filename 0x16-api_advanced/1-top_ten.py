8c4135010b1fcc64cf531b048b73807e54512516
jawadzizo <jadosss99@gmail.com>
2024-08-17 15:32:04 -0700

    add 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module to print 10 hotters subreddit's posts"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """function to print 10 hotters subreddit's posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    request_headers = {
+        "User-Agent": "jawad_alx"
+    }
+
+    response_headers = get(url, headers=request_headers, params={"limit": 10}, allow_redirects=False)
+    if response_headers.status_code == 404:
+        print("None")
+        return
+
+    results = response_headers.json()["data"]
+
+    for post in results["children"]:
+        print(post["data"]["title"])
