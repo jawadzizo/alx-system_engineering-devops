565bd7cfb2273a9c3ba9cd0d0ef4f6348a60c471
jawadzizo <jadosss99@gmail.com>
2024-08-26 16:18:29 -0700

    add 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function to find the total number of a subreddit's subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     request_headers = {
         "User-Agent": "jawad_alx"
     }
 
-    response_header = requests.get(url, headers=request_headers, allow_redirects=False)
-    if response_header.status_code == 404:
-        return 0
+    response_header = requests.get(url, headers=request_headers,
+                                   allow_redirects=False)
+    if response_header.status_code == 200:
+        return response_header.json().get("data").get("subscribers")
 
-    return response_header.json()["data"]["subscribers"]
+    return 0
