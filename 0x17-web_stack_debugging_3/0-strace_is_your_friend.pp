97fc1f42148879359aa4dc51668bf45017b09bcf
jawadzizo <jadosss99@gmail.com>
2024-08-27 12:09:10 -0700

    fix indentation

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # internal_error_fix
 exec { 'internal_error_fix':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => shell
 }
