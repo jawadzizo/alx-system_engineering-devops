206face7d1b2a85fc7de797bbfc6cfabc8e2d536
jawadzizo <jadosss99@gmail.com>
2024-08-27 14:46:41 -0700

    add 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,10 @@
 # nginx too many requests fix
-
 exec { 'nginx_too_many_requests_fix':
-  command => 'sed -i "s/15/1024/" /etc/default/nginx; service nginx restart',
+  command => 'sed -i "s/15/1024/" /etc/default/nginx',
+  path    => '/bin/: /usr/bin/'
+}
+
+exec { 'restart_nginx_server':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
